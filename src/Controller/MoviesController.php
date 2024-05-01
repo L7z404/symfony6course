63d8a3c12e3f431e4bb49a5bf9e7dec6cbae8763
L7z404 <leonelcastillo1014@gmail.com>
2024-04-30 18:30:19 -0600

    primer commit

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies', name: 'app_movies')]
+    public function index(): Response
+    {
+        $movies = ["Black Clover", "Breaking Bad", "Lucifer"];
+        return $this->render('index.html.twig', [
+            'movies' => $movies
+        ]);
+    }
+}
